aec493591a0a368470ff796bbd13f0d9650aaaa7
Sofia Luna <sofialunazx@gmail.com>
2021-07-19 03:10:18 -0600

    editar perfil y editar contraseña funcional

--- FrontEnd/fishingStudy-App/src/app/home-page/components/edit-profile/edit-profile.component.ts
+++ FrontEnd/fishingStudy-App/src/app/home-page/components/edit-profile/edit-profile.component.ts
@@ -13,42 +13,43 @@ import { Usuario } from '../../interfaces/interfaces';
 })
 export class EditProfileComponent implements OnInit{
 
-  formSubmitted:boolean = false;
-  name:any;
-  editForm!: FormGroup;
-  
-   
- 
+  user: Usuario = {
+    'uid':'',
+    'name':'',
+    'email':'',
+    'role':''
 
+  };
+  
+  editForm: FormGroup = this.fb.group({
+    name    :["",[Validators.required], []],
+    email   :["", [Validators.required,Validators.email], []],
+    role    :["", [Validators.required], []],
+    status  :["", [Validators.required], []],
+  });
   editPassword: FormGroup = this.fb.group({
     oldPassword     :["", [Validators.required], []],
     newPassword     :["", [Validators.required, Validators.pattern(this.authService.passwordRegex)], []],
     confirmPassword :["", [Validators.required], [this.authService.samePassword]]
   });
 
- 
 
   constructor(private fb : FormBuilder,
               private userService : UserService,
               private authService : AuthService          
     ) {
-     this.name = this.userService.getUser().subscribe(res => {res.name});
+    
     }
 
   ngOnInit(){
-
-    this.editForm = this.fb.group({
-      name    :[this.name,[Validators.required], []],
-      email   :[, [Validators.required,Validators.email], []],
-      role    :[, [Validators.required], []],
-      status  :[, [Validators.required], []],
+    this.userService.getUser().subscribe(res => {
+     this.user = res;
+      this.editForm.setValue({'name':res.name,'email':res.email,'role':res.role,'status':''})  
+      
     });
-
   }
-
+  
   updateUser(){
-
-    this.formSubmitted = true;
     this.userService.updateUser(this.editForm.value).
     subscribe( res => {
       if(res === true){
@@ -62,16 +63,29 @@ export class EditProfileComponent implements OnInit{
         Swal.fire("Se detecto un error al actualizar");
       }
     })
+   
   }
 
   updatePassword(){
-    this.formSubmitted = true;
-  }
-
-  clean(){
-    this.editForm.reset();
-    this.editForm.markAsPristine;
-    this.editForm.markAsTouched
+    this.userService.updatePassword(this.editPassword.value).
+    subscribe( res => {
+      if(res === true){
+        Swal.fire({
+          icon: 'success',
+          title: 'La contraseña se actualizo de forma exitosa',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }else{
+        Swal.fire({
+          icon: 'warning',
+          title : 'Se detecto un error al actualizar',
+          text:'Verifique su antigua contraseña y confirme la recien ingresada'
+        }
+          );
+      }
+    })
+   
   }
 
 }
