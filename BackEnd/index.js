d44e93362f288dd3ef41710c3d15f2726aab2498
Enki403 <vasquezlopezhectorjose@gmail.com>
2021-08-04 17:09:24 -0600

    Deploy Angular en Node

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,7 +3,8 @@
 require("dotenv").config(); //Cargamos las variables de entorno.
 const express = require('express'); //Framework express
 const cors = require("cors"); //
-const { dbConnection } = require('./database/config') //Conexion hacia la base de datos
+const path = require("path");
+const { dbConnection } = require('./database/config'); //Conexion hacia la base de datos
 
 // Paso 2
 // Crear el servidor/aplicacion de express
@@ -17,6 +18,9 @@ app.use(express.json()); //Transformamos los bodys en json, es una parseo del bo
 //Paso 4
 //Conexion a la BD.
 dbConnection();
+
+//Diirectorio publico
+app.use(express.static('public'));
 
 // app.get("/", (req,res)=>{
 //     res.json({
@@ -32,9 +36,13 @@ app.use("/api/login", require("./routes/login"));
 app.use("/api/upload", require("./routes/upload"));
 app.use("/api/markers", require("./routes/markers"));
 
+// Manejar cualquier otra ruta
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 // Paso final
 // Levantar el servico
 app.listen(process.env.PORT, () => {
-    console.log("Servidor corriendo en puerto " + process.env.PORT)
+    console.log("Servidor corriendo en puerto " + process.env.PORT);
 });
