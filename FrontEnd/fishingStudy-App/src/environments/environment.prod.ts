9ad36958914894f43b3c5824c0a2ff91f1637b02
JeanlucBoquin <jean.boquin.s@gmail.com>
2021-08-04 17:38:52 -0600

    Terminando las configuraciones del deploy a Heroku.

--- FrontEnd/fishingStudy-App/src/environments/environment.prod.ts
+++ FrontEnd/fishingStudy-App/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl:"http://localhost:4000/api",
+  baseUrl:"https://serfish.herokuapp.com/",
   mapboxToken:"pk.eyJ1IjoiamVhbmx1Yy1ib3F1aW4iLCJhIjoiY2tyb2h1M29uMGJhcjJ1cGcydmJ6eWkydyJ9.OBygfLwbOpdWJr8qbZF6tQ",
   openWeatherKey:"90b92f0c5ee2d6c71c554b178ff6e3f5"
 };
