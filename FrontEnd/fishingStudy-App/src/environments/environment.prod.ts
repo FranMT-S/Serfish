ca4a7ec70d1711424fda794ac1f183c4f718829c
FranMT-S <franciscomadridtoledo@gmail.com>
2023-01-04 03:14:45 -0600

    fran pro V1.0

--- FrontEnd/fishingStudy-App/src/environments/environment.prod.ts
+++ FrontEnd/fishingStudy-App/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl:"https://serfish.herokuapp.com/api",
+  baseUrl:"api",
   mapboxToken:"pk.eyJ1IjoiamVhbmx1Yy1ib3F1aW4iLCJhIjoiY2tyb2h1M29uMGJhcjJ1cGcydmJ6eWkydyJ9.OBygfLwbOpdWJr8qbZF6tQ",
   openWeatherKey:"90b92f0c5ee2d6c71c554b178ff6e3f5"
 };
